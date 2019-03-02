c0324ad9d84f54d1cdc0aee82cf5d2809e6c412d
GeorgiArn <georgearnaudov1@gmail.com>
2019-03-02 19:54:14 +0200

    Add Score Functionality

--- template.js
+++ template.js
@@ -19,6 +19,9 @@ function solve() {
         movingMultiplier: 4
     };
 
+    let score = 0;
+    let add = 0;
+
     function onGameStart() {
         gameStart.classList.add('hide');
         const snake = document.createElement("div");
@@ -32,25 +35,50 @@ function solve() {
 
     function gameAction() {
         const snake = document.querySelector(".snake"); 
+        const scoreBoard = document.querySelector(".game-score"); 
 
         if (keys.ArrowUp && player.y - 35 > 0) {
             player.y -= game.speed * game.movingMultiplier;
             snake.style.backgroundImage = "url('Images/SnakeHeadUp.png')";
+            score = score + 1;
+            if (score % 15 === 0) {
+                add = add + 1; 
+                let scoreNum = add;
+                scoreBoard.innerHTML = scoreNum;
+            }
         } 
 
         if (keys.ArrowDown && player.y + snake.offsetHeight - 25 < gameArea.offsetHeight) {
             player.y += game.speed * game.movingMultiplier;
             snake.style.backgroundImage = "url('Images/SnakeHeadDown.png')";
+            score = score + 1;
+            if (score % 15 === 0) {
+                add = add + 1; 
+                let scoreNum = add;
+                scoreBoard.innerHTML = scoreNum;
+            }
         }
 
         if (keys.ArrowLeft && player.x - 10 > 0) {
             player.x -= game.speed * game.movingMultiplier;
             snake.style.backgroundImage = "url('Images/SnakeHeadLeft.png')";
+            score = score + 1;
+            if (score % 15 === 0) {
+                add = add + 1; 
+                let scoreNum = add;
+                scoreBoard.innerHTML = scoreNum;
+            }
         }
 
         if (keys.ArrowRight && player.x + snake.offsetWidth + 5 < gameArea.offsetWidth) {
             player.x += game.speed * game.movingMultiplier;
             snake.style.backgroundImage = "url('Images/SnakeHeadRight.png')";
+            score = score + 1;
+            if (score % 15 === 0) {
+                add = add + 1; 
+                let scoreNum = add;
+                scoreBoard.innerHTML = scoreNum;
+            }
         }
 
         snake.style.top = player.y + "px";
