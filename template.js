fb705c6edc6efcc7c7d56f7364454807e5a44ea4
GeorgiArn <georgearnaudov1@gmail.com>
2019-03-02 18:58:33 +0200

    Add Button Click Event

--- template.js
+++ template.js
@@ -1,3 +1,13 @@
 function solve() {
-    console.log("hi");
+    
+    const gameScore = document.querySelector(".game-score"); 
+    const gameStart = document.querySelector(".game-start");
+    const gameArea = document.querySelector(".game-area");
+    const gameOver = document.querySelector(".game-over");
+    
+    gameStart.addEventListener('click', onGameStart);
+
+    function onGameStart() {
+        gameStart.classList.add('hide');
+    }
 }
